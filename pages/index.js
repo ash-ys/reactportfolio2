bea429b2ffe622661bb48e814c4f24860940e11b
Ashish Wagle <Awagle4@gmail.com>
2022-10-06 15:42:54 +0545

    r

--- pages/index.js
+++ pages/index.js
@@ -39,6 +39,17 @@ export default function Home() {
 
         </div>
        </section>
+       <section>
+        <div>
+          <h3 className="text-3xl py-1">Services I offer</h3>
+          <p className="text-md py-2 leading-8 text-gray-800">
+            Since the beginning of my journey as a freelance designer and developer,
+            I've done remote work for <span className="text-teal-500"> agencies</span> consulted for 
+            <span className="text-teal-500"> startups</span> and collaborated with talented people to create digital products for
+            both business and consumer use.
+          </p>
+        </div>
+       </section>
       </main>
 
       
